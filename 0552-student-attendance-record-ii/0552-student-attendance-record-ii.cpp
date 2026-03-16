5ca43383e93a7cd5c857611cee2abb6f8f373600
Mihnea Grigore <187482255+mihneagrigore@users.noreply.github.com>
2026-03-16 16:41:27 +0200

    Time: 31 ms (92.25%), Space: 13.1 MB (79.63%) - LeetHub

---
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -0,0 +1,27 @@
+#define MOD 1000000007LL
+
+class Solution {
+public:
+    int checkRecord(int n) {
+        long long dp[100005][2][3] = {0};
+        dp[1][0][0] = 1;
+        dp[1][1][0] = 1;
+        dp[1][0][1] = 1;
+
+        for(int i = 2; i <= n; ++i) {
+            dp[i][0][0] = (dp[i-1][0][0] % MOD + dp[i-1][0][1] % MOD + dp[i-1][0][2]  % MOD) % MOD ;
+            dp[i][1][0] = (dp[i-1][1][0] % MOD + dp[i-1][0][0] % MOD + dp[i-1][0][1] % MOD + dp[i-1][1][1] % MOD + dp[i-1][0][2] % MOD + dp[i-1][1][2] % MOD) % MOD ;
+            dp[i][0][1] = (dp[i-1][0][0] % MOD) % MOD ;
+            dp[i][1][1] = (dp[i-1][1][0] % MOD) % MOD ;
+            dp[i][0][2] = (dp[i-1][0][1] % MOD) % MOD ;
+            dp[i][1][2] = (dp[i-1][1][1] % MOD) % MOD; 
+        }
+
+        return (dp[n][0][0] % MOD  
+                 + dp[n][1][0] % MOD
+                 + dp[n][0][1] % MOD 
+                 + dp[n][1][1] % MOD 
+                 + dp[n][0][2] % MOD 
+                 + dp[n][1][2]% MOD ) % MOD;
+    }
+};
