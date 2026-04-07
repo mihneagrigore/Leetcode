8846e8ec789bdd817acee2ac1805fd1019405b5f
Mihnea Grigore <187482255+mihneagrigore@users.noreply.github.com>
2026-04-07 09:51:28 +0300

    Time: 831 ms (22.67%), Space: 11.5 MB (47.22%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,45 @@
+#include <cstring>
+
+class Solution {
+public:
+
+    bool check(string result, string word) {
+        if(result.size() > word.size())
+            return false;
+
+        return (result == word);
+    }
+
+    bool back(vector<vector<char>> &domain, string word, int i, int j, int k) {
+
+        if(k == word.size())
+            return true;
+
+       if(i < 0 || j < 0 || i >= domain.size() || j >= domain[i].size())
+        return false;
+
+        if(domain[i][j] != word[k])
+            return false;
+
+             char tmp = domain[i][j];
+             domain[i][j] = '#';
+             int ok = back(domain, word, i+1, j, k+1) ||
+                        back(domain, word, i-1, j, k+1) ||  
+                        back(domain, word, i, j + 1, k+1) ||
+                        back(domain, word, i, j - 1, k+1);
+            domain[i][j] = tmp;
+
+            if(ok)
+                return true;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i < board.size(); ++i) 
+            for(int j = 0; j < board[i].size(); ++j)
+                if(back(board, word, i, j, 0))
+                    return true;
+
+        return false;
+    }
+};
