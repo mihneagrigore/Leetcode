913b0a9ec209637e044dbc76978ad7bb00ffadaf
Mihnea Grigore <187482255+mihneagrigore@users.noreply.github.com>
2026-05-21 18:49:42 +0300

    Time: 7 ms (29.29%), Space: 18.4 MB (37%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>> adj[n];
-        for(auto it: flights) {
-            adj[it[0]].push_back({it[1], it[2]}); 
+        vector<vector<pair<int, int>>> adj(n+1);
+        
+        for(auto it : flights) {
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        
-        vector<int> dist(n, 1e9);
-        queue<pair<int,pair<int,int>>> q; 
-        
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<int> dist(n+1, INT_MAX);
+        pq.push({0, {src, 0} });
         dist[src] = 0;
-        q.push({0, {src, 0}}); 
-        
-        while(!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            
-            int stops = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
-            
+
+        while(!pq.empty()) {
+            int stops = pq.top().first, node = pq.top().second.first, cost = pq.top().    second.second;
+            pq.pop();
+
             if(stops > k) continue;
-            
-            for(auto iter: adj[node]) {
-                int adjNode = iter.first;
-                int edW = iter.second;  
-                
-                if(cost + edW < dist[adjNode] && stops <= k) {
-                    dist[adjNode] = cost + edW;
-                    q.push({stops + 1, {adjNode, cost + edW}});
+
+            for(auto neigh : adj[node]) {
+                int nw = neigh.second, nnode = neigh.first;
+                int newcost = nw + cost;
+
+                if(newcost < dist[nnode] && stops <= k) {
+                    dist[nnode] = newcost;
+                    pq.push({stops + 1, {nnode, dist[nnode]}});
                 }
             }
         }
-        
-        if(dist[dst] == 1e9) return -1;  
+
+        if(dist[dst] == INT_MAX)
+            return -1;
+
         return dist[dst];
     }
 };
